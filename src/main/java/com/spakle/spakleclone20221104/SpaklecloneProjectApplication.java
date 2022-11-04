c7481377a765ba2b5e914bfc632f3c49a04098c9
qorals1121 <qorals1121@naver.com>
2022-11-04 10:03:52 +0900

    첫시작

---
+++ src/main/java/com/spakle/spakleclone20221104/SpaklecloneProjectApplication.java
@@ -0,0 +1,13 @@
+package com.spakle.spakleclone20221104;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaklecloneProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaklecloneProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spakle/spakleclone20221104/SpaklecloneProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spakle.spakleclone20221104.spakleclone.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaklecloneProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
