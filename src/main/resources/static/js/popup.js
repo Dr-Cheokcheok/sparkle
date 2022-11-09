87026fab4fb7c61f9fa134f3649761a3ceda2611
qorals1121 <qorals1121@naver.com>
2022-11-09 16:08:58 +0900

    팝업올림

---
+++ src/main/resources/static/js/popup.js
@@ -0,0 +1,19 @@
+
+// 팝업 열기
+function openPop() {
+  document.getElementById("pop-box").style.display = "block";
+}
+
+// 팝업 닫기
+function closePop() {
+  document.getElementById("pop-box").style.display = "none";
+}
+
+function openPop2() {
+  document.getElementById("pop-box2").style.display = "block";
+}
+
+// 팝업 닫기
+function closePop2() {
+  document.getElementById("pop-box2").style.display = "none";
+}
