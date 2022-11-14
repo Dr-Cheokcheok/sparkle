5ed8f4adccf2bc98b591d57018e91876c9231479
code0715 <jungmin7318@gmail.com>
2022-11-14 16:23:07 +0900

    전체 카테고리 백그라운드

--- src/main/java/com/spakle/spakleclone20221104/controller/logInController.java
+++ src/main/java/com/spakle/spakleclone20221104/controller/logInController.java
@@ -7,13 +7,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class logInController {
 
     @GetMapping("/login")
-    public String loadlogin() {return "mypage/login";}
+    public String loadlogin() {return "login";}
 
     @GetMapping("/register")
-    public String loadregister() {return "mypage/register";}
+    public String loadregister() {return "register";}
 
     @GetMapping("/forgot")
-    public String loadforgot() {return "mypage/forgot";}
+    public String loadforgot() {return "forgot";}
 
     @GetMapping("/order")
     public String loadorder() {return "order";}
