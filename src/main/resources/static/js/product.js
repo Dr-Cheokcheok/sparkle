beba12375efd30829df2a8ad96acbf10b6a04c95
qorals1121 <qorals1121@naver.com>
2022-11-16 13:43:33 +0900

    상세상품페이지 데이터 뿌리기 중셉

--- src/main/resources/static/js/product.js
+++ src/main/resources/static/js/product.js
@@ -72,3 +72,106 @@ function quantityCalc() {
         quantityCalc();
     }
 }
+
+
+/* DB에서 데이터 받아오기 */
+
+class ProductApi {
+    static #instance = null;
+
+    static getInstance() {
+        if(this.#instance == null) {
+            this.#instance = new ProductApi();
+        }
+        return this.#instance;
+    }
+
+    getProduct() {
+        let responseData = null;
+        const uri = location.href;
+        const groupId = uri.substring(uri.lastIndexOf("/") + 1);
+
+        $.ajax({
+            async: false,
+            type: "get",
+            url: "/product/" + groupId,
+            dataType: "json",
+            success: (response) => {
+                responseData = response.data;
+            },
+            error: (error) => {
+                console.log(error);
+            }
+        });
+
+        return responseData;
+    }
+}
+
+class ProductDetailService {
+    static #instance = null;
+    static getInstance() {
+        if(this.#instance == null) {
+            this.#instance = new ProductDetailService();
+        }
+        
+        return this.#instance;
+    }
+
+    loadProductDetail() {
+        const responseData = ProductApi.getInstance().getProduct();
+        this.getProductImg(responseData.img);
+        this.getProductImgFiles(responseData.imgNames);
+        this.getProductInfo(responseData);
+        console.log(responseData);
+    }
+
+    getProductImg(img) {
+        const productImg = document.querySelector(".photo-box")
+        productImg.innerHTML += `
+        <img src="/image/product/${img}" alt="상품이미지">
+        `;
+    }
+
+    getProductImgFiles(imgNames) {
+        const productImgs = document.querySelector("#goodsInfo");
+        imgNames.forEach(imgName => {
+            productImgs.innerHTML += `
+            <div class="edit-box">
+                <p style="text-align: center;">
+                    <img src="/image/product/${imgName}">
+                </p>
+            </div>
+            `;
+        });
+    }
+
+    getProductInfo(responseData) {
+        const productTitle = document.querySelector(".name");
+        const productPrice = document.querySelector(".info-list-box");
+
+        productTitle.value = responseData.name;
+        productPrice.innerHTML += `
+        <dl class="info-list clear">
+            <dt class="gray">정가</dt>
+            <dd class="gray under">${responseData.price}원</dd>
+        </dl>
+        <dl class="info-list clear">
+            <dt>판매가</dt>
+            <dd class="bold spoqa">${responseData.retailPrice}원</dd>
+        </dl>
+        <dl class="info-list clear">
+            <dt>할인받은 금액</dt>
+            <dd class="blue spoqa">5,100 (${responseData.rate})</dd>
+        </dl>
+        <dl class="info-list clear">
+            <dt>배송</dt>
+            <dd>무료배송</dd>
+        </dl>
+        `;
+    }
+}
+
+window.onload = () => {
+    ProductDetailService.getInstance().loadProductDetail();
+}
