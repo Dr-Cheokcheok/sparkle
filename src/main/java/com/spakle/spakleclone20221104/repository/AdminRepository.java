061f6aec03e74bab2aab743770db7a0ab1495321
qorals1121 <qorals1121@naver.com>
2022-11-18 15:43:27 +0900

    test

--- src/main/java/com/spakle/spakleclone20221104/repository/AdminRepository.java
+++ src/main/java/com/spakle/spakleclone20221104/repository/AdminRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 @Mapper
 public class AdminRepository {
-    
+
 }
--- src/main/resources/static/js/shop/shop.js
+++ src/main/resources/static/js/shop/shop.js
@@ -1,84 +1,124 @@
-function changeCategory() {
-    const products = document.querySelector(".products");
-    const categoryBtns = document.querySelectorAll(".category-btn");
-    let responseData = null;
-    console.log("changGroup 실행");
-  
-    for (let i = 0; i < categoryBtns.length; i++) {
-  
-      if (categoryBtns[i].checked) {
-  
-        // products.innerHTML = "";
-        console.log("카테고리 선택 실행");
-        $.ajax({
-          async: false,
-          type: "get",
-          url: "/api/product/category/" + categoryBtns[i].value,
-          contentType: "application/json",
-          data: categoryBtns[i].value,
-          dataType: "json",
-          success: (response) => {
-            //선택 category 제품 list
-            responseData = response.data;
-            console.log(responseData);
-          },
-          error: (error) => {
-            console.log(error)
-          }
-        });
-      }
-  
+function checkCategory() {
+
+  const categoryBtns = document.querySelectorAll(".category-btn");
+  const uri = location.href; //버튼 누를때 마다 카테고리 가져옴
+  const category = decodeURI(uri.substring(uri.lastIndexOf("/") + 1));
+
+  categoryBtns.forEach(categoryBtn => {
+    if(category == categoryBtn.value){
+
+    categoryBtn.checked = true;
+    toggleBtn(categoryBtn.id);
     }
-  
-        const groupInputs = document.querySelectorAll(".sub-btn");
-        const subBtnLabel = document.querySelectorAll(".sub-button-label");
-  
-        groupInputs.forEach(input => {
-  
-          input.onclick = () => {
-            console.log("그룹인풋 클릭");
-  
-            $('.sub-btn').removeClass("on");
-            $(this).addClass("on");
-  
-            if (input.classList.contains("on") || input.checked) {
-              products.innerHTML = "";
-              responseData.forEach(product => {
-                if (product.group == input.value) {
-  
-                  products.innerHTML += `
-                
-                    <li class="product">
-                        <figure>
-                        <a href= "/product/${product.productId}">
-                            <img src="/image/product/${product.img}" alt="인기상품 이미지 01">
-                            <!----  best 아이템에만 figcaption  --->
-                                <figcaption>BEST</figcaption>
-                        </a>
-                            <div class="time-icon">
-                                <button type="button" class="like"><img src="/static/images/img/best-icon01.png" alt=""></button>
-                                <button type="button" class="bag"><img src="/static/images/img/best-icon02.png" alt=""></button>
-                            </div>
-                        </figure>
-                        <div class="product-text">
-                        <a href="/product/${product.productId}">${product.name}</a>
-                        <p class="price spoqa">
-                            <span class="blue">${product.rate}%</span>
-                            ${product.retailPrice}원
-                            <span class="gray spoqa">${product.price}원</span>
-                        </p>
-                        </div>
-                    </li>
-                    `;
-                }
-              });
-            }
-            //여기임
-          }
-        });
+  });
+}
+
+
+
+function toggleBtn(s) {
+  if(s == 'button1'){
+    document.getElementById('category1').checked = true;
+    document.getElementById('category_button1').style.display = "block";
+    document.getElementById('category_button2').style.display = "none";
+    document.getElementById('category_button3').style.display = "none";
+  }else if(s == 'button2'){
+    document.getElementById('category5').checked = true;
+    document.getElementById('category_button1').style.display = "none";
+    document.getElementById('category_button2').style.display = "block";
+    document.getElementById('category_button3').style.display = "none";
+  }else if(s == 'button3'){
+    document.getElementById('category6').checked = true;
+    document.getElementById('category_button1').style.display = "none";
+    document.getElementById('category_button2').style.display = "none";
+    document.getElementById('category_button3').style.display = "block";
   }
-  
-  
-  window.onload = () => {
-    changeCategory();
+  changeCategory();
+}
+
+
+let responseData = null;
+function changeCategory() {
+
+  const categoryBtns = document.querySelectorAll(".category-btn");
+
+  for(let i = 0 ; i < categoryBtns.length; i++){
+    if (categoryBtns[i].checked) {
+
+      // products.innerHTML = "";
+      $.ajax({
+        async: false,
+        type: "get",
+        url: "/api/product/category/" + categoryBtns[i].value,
+        contentType: "application/json",
+        data: categoryBtns[i].value,
+        dataType: "json",
+        success: (response) => {
+          //선택 category 제품 list
+          this.responseData = response.data;
+
+        },
+        error: (error) => {
+          console.log(error)
+        }
+      });
+
+    }
+
   }
+
+  changeGroup();
+}
+
+function changeGroup(){
+  const groupInputs = document.querySelectorAll(".sub-btn");
+
+  groupInputs.forEach(input => {
+
+    reload(input);
+    input.onclick = () => {
+      reload(input);
+    }
+  });
+}
+
+function reload(input){
+  const products = document.querySelector(".products");
+
+  if(input.checked){
+    products.innerHTML = "";
+    this.responseData.forEach(product => {
+      if (product.group == input.value) {
+
+        products.innerHTML += `
+    
+    <li class="product">
+      <figure>
+        <a href= "/product/${product.productId}">
+            <img src="/image/product/${product.img}" alt="인기상품 이미지 01">
+            <!----  best 아이템에만 figcaption  --->
+              <figcaption>BEST</figcaption>
+        </a>
+            <div class="time-icon">
+              <button type="button" class="like"><img src="/static/images/img/best-icon01.png" alt=""></button>
+              <button type="button" class="bag"><img src="/static/images/img/best-icon02.png" alt=""></button>
+            </div>
+      </figure>
+      <div class="product-text">
+        <a href="/product/${product.productId}">${product.name}</a>
+        <p class="price spoqa">
+            <span class="blue">${product.rate}%</span>
+            ${product.retailPrice}원
+            <span class="gray spoqa">${product.price}원</span>
+        </p>
+      </div>
+    </li>
+    `;
+      }
+    });
+  }
+}
+
+
+window.onload = () => {
+  checkCategory();
+}
